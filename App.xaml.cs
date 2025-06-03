512a5ce1f11a800c0d1a8dfecbe2f9b63876af8a
elisevgeniy <elisevgeniy@gmail.com>
2025-06-04 00:10:48 +0300

    refactor: rename namespaces

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace VTBpdfTOcsv
+namespace VTBpdfReportConverter
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using Microsoft.Win32;
 using VTBpdfReportConverter.Converter;
 
-namespace VTBpdfTOcsv
+namespace VTBpdfReportConverter
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
